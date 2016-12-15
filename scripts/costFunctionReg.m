d400692c15ca8fe73ebaf482b890fc9e5d0f186a
amandavinci@gmail.com <amanthevinci@gmail.com>
2016-12-15 13:29:04 +0530

    Implemented Regularized Logistic Regression Cost

--- scripts/costFunctionReg.m
+++ scripts/costFunctionReg.m
@@ -17,11 +17,43 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+% Number of features
+n = size(theta);
 
+% Computing the first summation in formula
+for i = 1:m,
+	z = X(i, :) * theta;
+	h = sigmoid(z);
+	J = J - y(i) * log(h) - (1 - y(i)) * log(1 - h);
+end
 
+% Simple Cost function
+J = J / m; 
 
+regParam = 0;
+% Computing the regularization parameter
+for i = 2:n,
+	regParam = regParam + theta(i) * theta(i);
+end
+% Regularization Parameter
+regParam = lambda * regParam / ( 2 * m);
 
+% Regularized Cost Function
+J = J + regParam;
 
+% Computing the gradient
+for j = 1:n,
+	for i = 1:m,
+		z = X(i, :) * theta;
+		h = sigmoid(z);
+		grad(j) = grad(j) + (h - y(i)) * X(i, j);
+	end
+
+	grad(j) = grad(j) / m;
+	% Regularization for theta greater than 1
+	if (j >= 2),
+		grad(j) = grad(j) + lambda * theta(j) / m;
+end
 % =============================================================
 
 end
